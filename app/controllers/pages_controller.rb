c7f1b10a75f611f1eb579729d779839040cc4e01
mikeya <mikeabell2@gmail.com>
2015-01-19 12:32:05 -0800

    qa work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,29 +1,55 @@
 class PagesController < ApplicationController
   def index
+    @title ='Sushirrito - The original Sushi Burrito concept!'
+    @description ='Sushirrito® - The original Sushi Burrito concept!  Revolutionizing sushi culture with made-to-order, hand-held Sushi Burritos in delicious Asian and Latin flavor combinations.'
+    @keywords ='sushirrito, sushi burrito, hand held sushi, sustainability-focused sushi, environmentally friendly sushi, sustainable fishing, eco-friendly sushi, eco friendly sushi'
   end
 
   def locations
+    @title ='Sushirrito - The original Sushi Burrito concept!'
+    @description ='Sushirrito® - The original Sushi Burrito concept!  Revolutionizing sushi culture with made-to-order, hand-held Sushi Burritos in delicious Asian and Latin flavor combinations.'
+    @keywords ='sushirrito, sushi burrito, hand held sushi, sustainability-focused sushi, environmentally friendly sushi, sustainable fishing, eco-friendly sushi, eco friendly sushi'
   end
 
   def food
+    @title ='Sushirrito - Sushi Burrito Rolls'
+    @description ='Made-to-order sushi burritos stuffed with savory Asian and Latin-infused ingredients and flavors. Introducing fast, fresh, filling and eco-friendly sushi'
+    @keywords ='Sushirrito menu, sushi burrito menu, Sushirritos, sustainability-focused sushi, environmentally friendly sushi, sustainable fishing, eco-friendly sushi, eco friendly sushi, safe harbor foods certified seafood, within fda standards, no hormones, no antibiotics, hormone free, antibiotic free'
   end
 
   def sf
+    @title ='Sushirrito - Sushi Burrito Rolls'
+    @description ='Made-to-order sushi burritos stuffed with savory Asian and Latin-infused ingredients and flavors. Introducing fast, fresh, filling and eco-friendly sushi'
+    @keywords ='Sushirrito menu, sushi burrito menu, Sushirritos, sustainability-focused sushi, environmentally friendly sushi, sustainable fishing, eco-friendly sushi, eco friendly sushi, safe harbor foods certified seafood, within fda standards, no hormones, no antibiotics, hormone free, antibiotic free'
   end
 
   def pa
+    @title ='Sushirrito - Sushi Burrito Rolls'
+    @description ='Made-to-order sushi burritos stuffed with savory Asian and Latin-infused ingredients and flavors. Introducing fast, fresh, filling and eco-friendly sushi'
+    @keywords ='Sushirrito menu, sushi burrito menu, Sushirritos, sustainability-focused sushi, environmentally friendly sushi, sustainable fishing, eco-friendly sushi, eco friendly sushi, safe harbor foods certified seafood, within fda standards, no hormones, no antibiotics, hormone free, antibiotic free'
   end
 
   def catering
-  end
+    @title ='Sushirrito - Catering'
+    @description ='Sushirrito offers online catering orders for your special gathering or corporate event.'
+    @keywords ='Sushirrito event catering, Sushirrito catering, sushi catering, sushi events, corporate parties, corporate events, sushi delivery'
 
-  def contact
   end
 
   def jobs
+    @title ='Sushirrito - Jobs'
+    @description ='Sushirrito is challenging conventional Japanese cuisine and revamping the current sushi landscape as we know it! We are seeking innovative and eager people to work within an exciting and ambitious environment.'
+    @keywords ='job, jobs, jobs at sushirrito, employment at sushirrito, employment san francisco, hiring san francisco, jobs in san francisco, sushi restaurant job san francisco, sushi restaurant job'
+
   end
 
   def about
+    @title ='Sushirrito - About Us'
+    @description ='Sushirrito is the original sushi burrito concept. We’re revolutionizing sushi culture with made-to-order, hand-held sushi burritos in delicious Asian and Latin flavor combinations.'
+    @keywords ='Sushirrito Story, Sushirrito Brand'
+
   end
+
+
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def cp(path)
     "current" if current_page?(path)
   end
+
+  def set_title(title = "Sushirrito")
+    content_for :title, title
+  end
 end
